6a0c5d912beea5d90cd4391f9d4d748ae0f6b31c
stravid87 <ravi.student.dev@gmail.com>
2025-06-01 11:01:11 -0400

    persister is now working

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,36 @@
 import './assets/main.css'
-import { VueQueryPlugin } from '@tanstack/vue-query'
-import { createApp } from 'vue'
-import app from './App.vue'
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
+import { persistQueryClient } from '@tanstack/query-persist-client-core';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import { createApp } from 'vue';
+import app from './App.vue';
 
-createApp(app).use(VueQueryPlugin).mount('#app');
+// 2. Create a new QueryClient instance
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
+
+// 3. Create a persister instance
+const localStoragePersister = createSyncStoragePersister({
+  storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+  key: 'ravisQueryCache',
+});
+
+// 4. Call persistQueryClient to link your QueryClient with the persister
+persistQueryClient({
+  queryClient, // The QueryClient instance to persist
+  persister: localStoragePersister, // The persister to use
+  maxAge: 1000 * 60 * 60 * 24, // 24 hours in milliseconds
+  onError: (error) => {
+    console.error('Persistence error:', error);
+  },
+});
+
+// #5
+createApp(app).use(VueQueryPlugin, {queryClient}).mount('#app');
 
